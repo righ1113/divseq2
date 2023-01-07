8132911e61a8d6df278f9a54c4dceebde53bbbb9
righ1113 <34955220+righ1113@users.noreply.github.com>
2023-01-07 22:42:11 +0900

    23/01/07 open Nat

--- Divseq2.lean
+++ Divseq2.lean
@@ -2,22 +2,21 @@ import Mathlib.Tactic.LibrarySearch
 
 #eval Lean.versionString
 
+open Nat
 
-namespace Nat
-  axiom p₁ (k : Nat) :  (succ k) * 2 = succ (succ (k * 2)) 
-  inductive Parity : Nat → Prop where
-    | even : (k : Nat) → Parity (k * 2)
-    | odd  : (k : Nat) → Parity (succ (k * 2))
-  inductive Mod3 : Nat → Prop where
-    | threeZero : (k : Nat) → Mod3 (k * 3)
-    | threeOne  : (k : Nat) → Mod3 (1 + k * 3)
-    | threeTwo  : (k : Nat) → Mod3 (2 + k * 3)
-  def parity (n : Nat) : Parity n := match n with
-    | 0      => Parity.even 0
-    | succ n => match (parity n) with
-      | Parity.even k  => Parity.odd k
-      | Parity.odd k   => by rw[←p₁]; exact Parity.even (succ k)
-end Nat
+axiom p₁ (k : Nat) :  (succ k) * 2 = succ (succ (k * 2)) 
+inductive Parity : Nat → Prop where
+  | even : (k : Nat) → Parity (k * 2)
+  | odd  : (k : Nat) → Parity (succ (k * 2))
+inductive Mod3 : Nat → Prop where
+  | threeZero : (k : Nat) → Mod3 (k * 3)
+  | threeOne  : (k : Nat) → Mod3 (1 + k * 3)
+  | threeTwo  : (k : Nat) → Mod3 (2 + k * 3)
+def parity (n : Nat) : Parity n := match n with
+  | 0      => Parity.even 0
+  | succ n => match (parity n) with
+    | Parity.even k  => Parity.odd k
+    | Parity.odd k   => by rw[←p₁]; exact Parity.even (succ k)
 
 def allDivSeq (x:Nat) : List (List Integer) := [[]] -- TBD
 
@@ -46,52 +45,6 @@ inductive SingleLimited : Nat → Prop where
   | isSingleLimited12 : (l : Nat) → (ExtsLimited <| 2*l) → (SingleLimited <| 18*l+6)
   | isSingleLimited13 : (l : Nat) → (ExtsLimited <| 4*l+1) → (SingleLimited <| 18*l+12)
   | isSingleLimited14 : (l : Nat) → (ExtsLimited <| 8*l+7) → (SingleLimited <| 18*l+18)
-
-axiom h₆ (l : Nat) : (16 * l + 3) + (16 * l + 3 - 3) / 8 + 1 = 18 * l + 4
-/-!
-theorem singleToExts (n : Nat) (p : SingleLimited n) : ExtsLimited n :=
-  match p with
-    | SingleLimited.isSingleLimited02 l p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ p3 _ _ _ _ _ => p3
-    | SingleLimited.isSingleLimited03 m p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ _ _ _ p3 _ _ => by rw [←Nat.mul_assoc 18 2 m] at p3; simp[p3]
-    | SingleLimited.isSingleLimited04 m p2 => sorry
-    | SingleLimited.isSingleLimited05 m p2 => sorry
-    | SingleLimited.isSingleLimited06 l p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ _ _ _ _ _ p3 => have p4 := Eq.subst (h₆ l) p3; p4
-    | SingleLimited.isSingleLimited07 l p2 => sorry
-    | SingleLimited.isSingleLimited08 l p2 => sorry
-    | SingleLimited.isSingleLimited09 l p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ _ p3 _ _ _ _ => p3
-    | SingleLimited.isSingleLimited11 k p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ _ _ p3 _ _ _ => p3
-    | SingleLimited.isSingleLimited12 l p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ _ _ _ _ p3 _ => by rw [←Nat.mul_assoc 9 2 l] at p3; simp[p3]
-    | SingleLimited.isSingleLimited13 l p2 => sorry
-    | SingleLimited.isSingleLimited14 l p2 => sorry
--/
-theorem singleToExts2 (n : Nat) (p : SingleLimited (Nat.succ (Nat.succ n))) : ExtsLimited (n+2) :=
-  match p with
-    | SingleLimited.isSingleLimited02 l p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ p3 _ _ _ _ _ => p3
-    | SingleLimited.isSingleLimited03 m p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ _ _ _ p3 _ _ => by rw [←Nat.mul_assoc 18 2 m] at p3; simp[p3]
-    | SingleLimited.isSingleLimited04 m p2 => sorry
-    | SingleLimited.isSingleLimited05 m p2 => sorry
-    | SingleLimited.isSingleLimited06 l p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ _ _ _ _ _ p3 => have p4 := Eq.subst (h₆ l) p3; p4
-    | SingleLimited.isSingleLimited07 l p2 => sorry
-    | SingleLimited.isSingleLimited08 l p2 => sorry
-    | SingleLimited.isSingleLimited09 l p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ _ p3 _ _ _ _ => p3
-    | SingleLimited.isSingleLimited11 k p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ _ _ p3 _ _ _ => p3
-    | SingleLimited.isSingleLimited12 l p2 => match p2 with
-      | ExtsLimited.isExtsLimited09 _ _ _ _ _ p3 _ => by rw [←Nat.mul_assoc 9 2 l] at p3; simp[p3]
-    | SingleLimited.isSingleLimited13 l p2 => sorry
-    | SingleLimited.isSingleLimited14 l p2 => sorry
-
-theorem hoge (l:Nat) (p : SingleLimited <| 18*l+6) : (ExtsLimited <| 18*l+6) := singleToExts2 (18 * l + 4) p
 
 
 
--- Main.lean
+++ Main.lean
@@ -3,15 +3,38 @@
 
 import «Divseq2»
 
+open Nat
 
-namespace Nat
-  def gcdF3 (x : Nat) : (∀ x₁, x₁ < x → SingleLimited x₁) → SingleLimited x :=
-    match x with
-    | 0      => fun _ => sorry
-    | succ x => fun f => have foo := f 0 (Nat.lt_of_sub_eq_succ rfl); sorry --f (7 % succ x) (mod_lt _ (zero_lt_succ  _))
-  def gcd3 (n : Nat) : SingleLimited n :=
-    WellFounded.fix' (measure id).wf gcdF3 n
-end Nat
+axiom h₆ (l : Nat) : (16 * l + 3) + (16 * l + 3 - 3) / 8 + 1 = 18 * l + 4
+theorem singleToExts (n : Nat) (p : SingleLimited (succ (succ n))) : ExtsLimited (n+2) :=
+  match p with
+    | SingleLimited.isSingleLimited02 l p2 => match p2 with
+      | ExtsLimited.isExtsLimited09 _ p3 _ _ _ _ _ => p3
+    | SingleLimited.isSingleLimited03 m p2 => match p2 with
+      | ExtsLimited.isExtsLimited09 _ _ _ _ p3 _ _ => by rw [←Nat.mul_assoc 18 2 m] at p3; simp[p3]
+    | SingleLimited.isSingleLimited04 m p2 => sorry
+    | SingleLimited.isSingleLimited05 m p2 => sorry
+    | SingleLimited.isSingleLimited06 l p2 => match p2 with
+      | ExtsLimited.isExtsLimited09 _ _ _ _ _ _ p3 => have p4 := Eq.subst (h₆ l) p3; p4
+    | SingleLimited.isSingleLimited07 l p2 => sorry
+    | SingleLimited.isSingleLimited08 l p2 => sorry
+    | SingleLimited.isSingleLimited09 l p2 => match p2 with
+      | ExtsLimited.isExtsLimited09 _ _ p3 _ _ _ _ => p3
+    | SingleLimited.isSingleLimited11 k p2 => match p2 with
+      | ExtsLimited.isExtsLimited09 _ _ _ p3 _ _ _ => p3
+    | SingleLimited.isSingleLimited12 l p2 => match p2 with
+      | ExtsLimited.isExtsLimited09 _ _ _ _ _ p3 _ => by rw [←Nat.mul_assoc 9 2 l] at p3; simp[p3]
+    | SingleLimited.isSingleLimited13 l p2 => sorry
+    | SingleLimited.isSingleLimited14 l p2 => sorry
+theorem hoge (l:Nat) (p : SingleLimited <| 18*l+6) : (ExtsLimited <| 18*l+6) := singleToExts (18 * l + 4) p
+
+def makeLimitedDivSeq (x : Nat) : (∀ x₁, x₁ < x → SingleLimited x₁) → SingleLimited x :=
+  match x with
+  | 0      => fun _ => sorry
+  | succ x => fun f => have foo := f 0 (Nat.lt_of_sub_eq_succ rfl); sorry --f (7 % succ x) (mod_lt _ (zero_lt_succ  _))
+-- 最終的な定理
+def LimitedDivSeq (n : Nat) : SingleLimited n :=
+  WellFounded.fix' (measure id).wf makeLimitedDivSeq n
 
 
 namespace Nat
